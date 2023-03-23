98a7348cee9a33c8338078a14b617a37e4649cc8
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-23 12:29:44 +0100

    jsnack 2 map + charAt + slice

---
+++ jsnack2/main.js
@@ -0,0 +1,15 @@
+/*
+
+Snack 2
+
+A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+
+Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]
+
+*/
+
+const wordsArray1 = [ 'pippo', 'PlUTO', 'Paperino'];
+
+const wordsArray2 = wordsArray1.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+
+console.log(wordsArray2);
