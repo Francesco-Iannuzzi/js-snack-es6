d05767df20b890f3c91bdecb311093f0eb5007e9
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-23 13:02:22 +0100

    jsnack3

---
+++ jsnack3/main.js
@@ -0,0 +1,27 @@
+/*
+
+snack 3
+Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+
+[
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+
+
+Crea un nuovo array con la lista dei mammiferi.
+
+*/
+
+const zoo = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+];
+
+
+const mammiferi = zoo.filter((animal) => animal.classe == 'mammiferi');
+
+console.log(mammiferi);
